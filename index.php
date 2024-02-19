c74f3ef2752e6f7466680eee266f6b7dc405095d
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-19 14:02:45 +0100

    edit

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
 <?php 
+
+class Genre {
+    public $name;
+
+    public function __construct ($nome) {
+        $this->name = $nome;
+    }
+
+}
 class Movie {
     public $title;
     public $releaseDate;
-    private $genre = array();
+    public $director;
+    public $duration;
+    public $availableInStreaming;
+    private $genre; // se è privata hai completo accesso a scrittura e lettura
 
-    function __construct($titolo, $dataRilascio, $genere) {
+    function __construct(
+        string $titolo,
+        $dataRilascio,
+        $regista,
+        $durata,
+        bool $visibileInStreaming = false,
+        Genre $genere )
+    {
         $this->title = $titolo;
         $this->releaseDate = $dataRilascio;
+        $this->director = $regista;
+        $this->duration = $durata;
+        $this->availableInStreaming = $visibileInStreaming;
         $this->setGenre($genere); 
     }
 
@@ -14,23 +36,30 @@ class Movie {
         return $this->genre;
     }
     public function setGenre($genere) {
-        $this->genre = $genere;
+        if (get_class($genere) == 'Genre') {
+            $this->genre = $genere;
+        }
+        else {
+            echo '<h2 style="color: red"> ERRORE: GENRE IS NOT FROM THE CLASS GENRE </h2>';
+        }
     }
 
 }
 
-$ratauille = new Movie('Ratauille', 'June 29, 2007', 'Fantasy');
+$fantasyGenre = new Genre('Fantasy');
+
+$ratauille = new Movie('Ratauille', 'June 29, 2007', 'Disney', '1h30', true, $fantasyGenre);
 
 var_dump($ratauille);
 
-echo 'Title: '.$ratauille->title = 'Ratatuille'.' - Release date: '.$ratauille->releaseDate = 'June 29, 2007'.' - Genre: '.$ratauille->setGenre('Fantasy');
+// echo 'Title: '.$ratauille->title = 'Ratatuille'.' - Release date: '.$ratauille->releaseDate = 'June 29, 2007'.' - Director: '.$ratauille->director = 'Disney'.' Duration: '.$ratatuille->duration = '1h.30'.' - Available in streaming: '.$ratauille->availableInStreaming = true.' - Genre: '.$ratauille->setGenre('Fantasy')->name;
 
 
 
 
 
-$tombRaider = new Movie('Tomb Raider', 'March 5, 2013', 'Action');
+$tombRaider = new Movie('Tomb Raider', 'March 5, 2013', 'Wes Ball', '2h30', false, new Genre('Action'));
 
 var_dump($tombRaider);
 
-echo 'Title: '.$tombRaider->title = 'Tomb Raider'.' - Release date: '.$tombRaider->releaseDate = 'March 5, 2013'.' - Genre: '.$tombRaider->setGenre('Action');
+// echo 'Title: '.$tombRaider->title = 'Tomb Raider'.' - Release date: '.$tombRaider->releaseDate = 'March 5, 2013'.' - Director: '.$tombRaider->director = 'Wes Ball'.' - Duration: '.$tombRaider->duration = '2h.30'.' - Available in streaming: '.$tombRaider->availableInStreaming = false.' - Genre: '.$tombRaider->setGenre('Action')->name;
